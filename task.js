de96b9385d78539d747241d198d57eb5bd2daa97
SergeyESh <SergeyESh1989@yandex.ru>
2023-02-03 10:41:08 +0300

    First commit

---
+++ task.js
@@ -0,0 +1,9 @@
+function compareArrays(arr1, arr2) {
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+  return result; // boolean
+}
+
+function advancedFilter(arr) {
+  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
+  return resultArr; // array
+}
